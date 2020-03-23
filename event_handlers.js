ad3009aaa61cbd3e19f3c50fed0500d45acc1cad
istarostin <is.starostin@jet.msk.su>
2020-03-23 20:15:28 +0300

    code cleanup

--- event_handlers.js
+++ event_handlers.js
@@ -7,43 +7,36 @@ const counter = () => {
     }
 }
 
-let aCounter = counter();
+const aCounter = counter();
 
-let dialogIsActive = false;
 let playerName;
 
-const image = document.querySelector("#menu-image");
-image.onclick = function(e) {
-    nextStep(e);
-}
+document.querySelector("#menu-image").addEventListener("click", nextStep);
 
 function nextStep(e) {
-    if (dialogIsActive) {
-        return;
-    }
     let currentCount = aCounter.next();
     console.log("step number " + currentCount);
-    switch(currentCount) {
+    switch (currentCount) {
         case 0:
             replaceImage(e.target, "assets/jiub.png");
             break;
         case 1:
-            showDialog(text.greeting);
+            showSpeech(QUOTES.greeting);
             break;
         case 2:
-            hideDialog();
+            hideSpeech();
             askName();
             break;
         case 3:
-            showDialog(text.storm);
+            showSpeech(QUOTES.storm);
             break;
         case 4:
-            hideDialog();
+            hideSpeech();
             replaceImage(e.target, "assets/guard_coming.png");
-            showDialog(text.guardComes);
+            showSpeech(QUOTES.guardComes);
             break;
         case 5:
-            hideDialog();
+            hideSpeech();
             break;
     }
 }
@@ -51,70 +44,70 @@ function nextStep(e) {
 function replaceImage(oldImage, newImagePath) {
     let newImage = new Image();
     newImage.id = "menu-image";
-    newImage.src = newImagePath;   
-    newImage.onload = function() {
-        console.log(`image ${newImagePath} finished loading`);
-        newImage.addEventListener("click", (e) => {
-            nextStep(e);
-        });
-    };
-    console.log("starting loading image " + newImagePath);
+    newImage.src = newImagePath;
+    newImage.addEventListener("load", () => {
+        newImage.addEventListener("click", nextStep);
+    });
     let parent = oldImage.parentNode;
     parent.insertBefore(newImage, oldImage);
     parent.removeChild(oldImage);
-    
 }
 
-function showDialog(text) {
-    let dialog = document.querySelector(".dialog-wrapper");    
-    dialog.innerHTML = `<p>${text}</p>`;
-    dialog.style.visibility = "visible";
+function showSpeech(text) {
+    let speechBox = document.querySelector(".dialog-wrapper");
+    speechBox.innerHTML = `<p>${text}</p>`;
+    speechBox.style.visibility = "visible";
 }
 
-function hideDialog() {
-    let dialog = document.querySelector(".dialog-wrapper");    
+function hideSpeech() {
+    let dialog = document.querySelector(".dialog-wrapper");
     dialog.innerHTML = "";
     dialog.style.visibility = "hidden";
 }
 
 function askName() {
-    dialogIsActive = true;
-    let wrapper = document.querySelector(".wrapper");
+    // prevent clicking to next steps without entering name
+    const image = document.querySelector("#menu-image");
+    image.removeEventListener("click", nextStep);
+
     let customDialog = document.createElement("div");
     customDialog.className = "dialog output sexy-border";
     customDialog.id = "ask-name-dialog";
-    customDialog.innerHTML = customDialogElement();    
+    customDialog.innerHTML = customDialogElement();
+
+    let wrapper = document.querySelector(".wrapper");
     wrapper.appendChild(customDialog);
 
     let nameInput = document.querySelector("#name-input");
-    nameInput.oninput = function(e) {
-        console.log("enabling button");
-        document.querySelector("#name-button").disabled = false;
-        nameInput.oninput = null;
-    }
+    nameInput.addEventListener(
+        "input",
+        () => {
+            console.log("enabling button");
+            document.querySelector("#name-button").disabled = false;
+        },
+        { once: true }
+    );
 
     let nameButton = document.querySelector("#name-button");
-    nameButton.onclick = function(e) {
+    nameButton.addEventListener("click", () => {
         playerName = nameInput.value;
         console.log("player name is " + playerName);
         wrapper.removeChild(customDialog);
-        dialogIsActive = false;
+        image.addEventListener("click", nextStep);
         image.click();
-    }
-
+    });
 }
 
 function customDialogElement() {
-    let dialog = 
-    `<div class="dialog-header">
-        <p>Name</p>
-    </div>
-    <div class="dialog-body">
-        <input type="text" id="name-input" class="input sexy-border">
-    </div>
-     <div class="dialog-footer">
-        <button id="name-button" class="sexy-border" disabled>OK</button>
-     </div>
-    `;
+    let dialog =
+        `<div class="dialog-header">
+            <p>Name</p>
+        </div>
+        <div class="dialog-body">
+            <input type="text" id="name-input" class="input sexy-border">
+        </div>
+        <div class="dialog-footer">
+            <button id="name-button" class="sexy-border" disabled>OK</button>
+        </div>`;
     return dialog;
 }
--- strings.js
+++ strings.js
@@ -1,4 +1,4 @@
-const text = {
+const QUOTES = {
     greeting: "Stand up, there you go. You were dreaming. What's your name?",
     storm: "Well, not even last night's storm could wake you. I heard them say we've reached Morrowind. I'm sure they'll let us go.",
     guardComes: "Quiet, here comes the guard."
